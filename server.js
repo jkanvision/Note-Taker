44454aea8cd28636eaecbf7da3f4c285c6354c7e
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-30 22:54:18 -0400

    creates index.js and notes.js; initial edits to server.js and index.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// set up listener
+app.listen(PORT, () =>
+  console.log(`Listening at http://localhost:${PORT}`)
+);
