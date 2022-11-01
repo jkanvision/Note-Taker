d3b3291ebc16a08a8ee523376e32f15b49b9becf
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-31 22:33:10 -0400

    edits to port command for heroku; edits to notes.js to enable save

--- routes/notes.js
+++ routes/notes.js
@@ -10,8 +10,19 @@ notes.get('/', (req, res) => {
 // POST Route for a new note
 notes.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully`);
+
+    const {title, text} = req.body;
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+      };
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+    
 });
 
 module.exports = notes;
